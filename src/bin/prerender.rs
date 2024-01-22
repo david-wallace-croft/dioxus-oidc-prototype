0d80b54c1c596672b8c3572901fce57fa37ea36b
David Wallace Croft <david@croftsoft.com>
2024-01-22 17:30:17 -0600

    map path

--- src/bin/prerender.rs
+++ src/bin/prerender.rs
@@ -1,5 +1,6 @@
 use ::dioxus_fullstack::prelude::*;
 use ::dioxus_fullstack::router::FullstackRouterConfig;
+use ::std::path::PathBuf;
 use dioxus_oidc_prototype::route::Route;
 
 const DIST: &str = "dist";
@@ -8,7 +9,7 @@ const DIST: &str = "dist";
 async fn main() {
   let fullstack_router_config = FullstackRouterConfig::<Route>::default();
   let incremental_renderer_config =
-    IncrementalRendererConfig::default().static_dir(DIST);
+    IncrementalRendererConfig::default().map_path(map_path).static_dir(DIST);
   let serve_config: ServeConfig<FullstackRouterConfig<Route>> =
     ServeConfigBuilder::new_with_router(fullstack_router_config)
       .assets_path(DIST)
@@ -17,4 +18,20 @@ async fn main() {
   pre_cache_static_routes_with_props(&serve_config)
     .await
     .unwrap();
+}
+
+fn map_path(route: &str) -> PathBuf {
+  println!("route: {}", route);
+  let query_index_option: Option<usize> = route.find('?');
+  let route2: String = match query_index_option {
+    Some(query_index) => route[..query_index].to_owned(),
+    None => route.to_owned(),
+  };
+  let mut path = PathBuf::from(DIST);
+  for segment in route2.split_terminator('/') {
+    println!("segment: {}", segment);
+    path.push(segment);
+  }
+  println!("path: {}", path.display());
+  path
 }
