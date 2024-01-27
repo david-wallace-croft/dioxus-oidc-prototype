d92155e9881313c15e502bc7fecf7db206a5a0fb
David Wallace Croft <david@CroftSoft.com>
2024-01-27 11:09:25 -0600

    client init

--- src/components/login_logout/mod.rs
+++ src/components/login_logout/mod.rs
@@ -1,13 +1,46 @@
+use std::future::Future;
+
+use self::errors::Error;
+use self::oidc::{
+  authorize_url, init_oidc_client, AuthRequest, AuthRequestState,
+  AuthTokenState, ClientState,
+};
+use self::props::client::ClientProps;
+use self::query::LoginQuerySegments;
 use ::dioxus::prelude::*;
+use ::openidconnect::core::CoreClient;
+use ::openidconnect::ClientId;
+
+mod constants;
+mod errors;
+mod oidc;
+mod props;
+mod query;
 
 #[allow(non_snake_case)]
 pub fn LoginLogout(cx: Scope) -> Element {
+  let use_state_auth_request_state: &UseState<AuthRequestState> =
+    use_state(cx, || AuthRequestState {
+      auth_request: None,
+    });
+  let use_state_auth_token_state: &UseState<AuthTokenState> =
+    use_state(cx, || AuthTokenState {
+      id_token: None,
+      refresh_token: None,
+    });
+  let use_state_client_state: &UseState<ClientState> =
+    use_state(cx, || ClientState {
+      oidc_client: None,
+    });
   render! {
   div {
     class: "app-login-logout",
   // TODO: show logout button if already logged in
   button {
-    onclick: move |_event| on_click(cx),
+    onclick: move |_event| on_click_login(
+      cx,
+      use_state_auth_request_state,
+      use_state_client_state),
     r#type: "button",
   "Login"
   }
@@ -15,6 +48,21 @@ pub fn LoginLogout(cx: Scope) -> Element {
   }
 }
 
-fn on_click(cx: Scope) {
+fn on_click_login(
+  cx: Scope,
+  _use_state_auth_request_state: &UseState<AuthRequestState>,
+  _use_state_client_state: &UseState<ClientState>,
+) {
   log::info!("Clicked");
+  use_future(cx, (), |_| async move { on_click_login_async().await });
+  // let option: Option<&Result<(ClientId, CoreClient), Error>> =
+  //   init_client_future.value();
+}
+
+async fn on_click_login_async() {
+  let result: Result<(ClientId, CoreClient), Error> = init_oidc_client().await;
+  if result.is_err() {
+    let error: Error = result.err().unwrap();
+    log::error!("{error}");
+  }
 }
