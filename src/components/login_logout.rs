61594a25876be09f73cd0c16ac97cb2b4bdd2330
David Wallace Croft <david@CroftSoft.com>
2024-01-27 09:58:43 -0600

    clicked

--- src/components/login_logout.rs
+++ src/components/login_logout.rs
@@ -5,11 +5,16 @@ pub fn LoginLogout(cx: Scope) -> Element {
   render! {
   div {
     class: "app-login-logout",
+  // TODO: show logout button if already logged in
   button {
-    class: "app-login-logout",
+    onclick: move |_event| on_click(cx),
     r#type: "button",
-  "Login / Logout Button"
+  "Login"
   }
   }
   }
+}
+
+fn on_click(cx: Scope) {
+  log::info!("Clicked");
 }
