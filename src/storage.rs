4c7cbfa7da44318ecdbb4f5da668cfb1d03e285c
David Wallace Croft <david@CroftSoft.com>
2024-02-26 19:27:05 -0600

    local storage

--- src/storage.rs
+++ src/storage.rs
@@ -1,17 +1,17 @@
 use crate::components::login_logout::constants;
 use crate::log::LogId;
 use gloo_storage::errors::StorageError;
-use gloo_storage::{SessionStorage, Storage};
+use gloo_storage::{LocalStorage, Storage};
 
 pub fn pkce_verifier_delete() {
   log::info!("{} Deleting PKCE verifier from storage...", LogId::L018);
-  SessionStorage::delete(constants::STORAGE_KEY_PKCE_VERIFIER);
+  LocalStorage::delete(constants::STORAGE_KEY_PKCE_VERIFIER);
 }
 
 pub fn pkce_verifier_get() -> Option<String> {
   log::info!("{} Load PKCE verifier from storage...", LogId::L004);
   let pkce_verifier_result: Result<String, StorageError> =
-    SessionStorage::get(constants::STORAGE_KEY_PKCE_VERIFIER);
+    LocalStorage::get(constants::STORAGE_KEY_PKCE_VERIFIER);
   match pkce_verifier_result {
     Ok(pkce_verifier) => {
       log::info!("{} PKCE verifier: {pkce_verifier}", LogId::L005);
@@ -26,7 +26,7 @@ pub fn pkce_verifier_get() -> Option<String> {
 
 pub fn pkce_verifier_set(pkce_verifier: &str) {
   let result: Result<(), StorageError> =
-    SessionStorage::set(constants::STORAGE_KEY_PKCE_VERIFIER, pkce_verifier);
+    LocalStorage::set(constants::STORAGE_KEY_PKCE_VERIFIER, pkce_verifier);
   match result {
     Ok(_) => {
       log::info!("{} PKCE Verifier stored successfully", LogId::L016)
